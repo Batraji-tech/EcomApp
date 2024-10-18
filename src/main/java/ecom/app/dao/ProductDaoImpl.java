453093bb8d826fe750747cfd46766836d35a2897
Harsh Batra <harshbatra2255@gmail.com>
2024-10-18 23:08:41 +0530

    check1

--- src/main/java/ecom/app/dao/ProductDaoImpl.java
+++ src/main/java/ecom/app/dao/ProductDaoImpl.java
@@ -212,20 +212,20 @@ import org.springframework.jdbc.core.JdbcTemplate;
  
 		@Override
 		public List<Category> getAllProductsByCategory() {
-			String sql = "SELECT * FROM category1 LIMIT 3 ";
+			String sql = "SELECT * FROM category LIMIT 3 ";
 		    List<Category> categoryList = jdbcTemplate.query(sql, new CategoryRowMapper());
 		    return categoryList;
 		}
 	   
 		@Override
 		public List<Category> getAllProductsByCategory1() {
-			String sql = "SELECT * FROM category1";
+			String sql = "SELECT * FROM category";
 		    List<Category> categoryList = jdbcTemplate.query(sql, new CategoryRowMapper());
 		    return categoryList;
 		}
 		@Override
 		public List<Category> getAllCategories() {
-		    String sql = "SELECT * FROM category1";
+		    String sql = "SELECT * FROM category";
 		    return jdbcTemplate.query(sql, new CategoryRowMapper()); // Adjust as per your implementation
 		}
 		
